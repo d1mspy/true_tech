45201b13ccf184ef193702c378cf386807e8a96d
qwertiek <119134870+qwertiek@users.noreply.github.com>
2025-04-20 00:13:35 +0300

    add front

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -17,24 +17,67 @@
 			responseText = 'Ошибка при POST-запросе';
 		}
 	}
-
-	async function sendGet() {
-		try {
-			const res = await fetch('/api/test');
-			const json = await res.json();
-			responseText = JSON.stringify(json);
-		} catch (err) {
-			responseText = 'Ошибка при GET-запросе';
-		}
-	}
 </script>
 
-<div style="display: flex; gap: 0.5rem; align-items: center;">
-	<input bind:value={inputValue} placeholder="Введите что-нибудь" />
-	<button on:click={sendPost}>Post</button>
+<div class="main">
+	<h2>Hi, send me a JSON file</h2>
+	<div class="input-container">
+		<input bind:value={inputValue} placeholder="Введите что-нибудь" />
+		<button on:click={sendPost} class="send-button"><img src="./img/send.png" alt="" /></button>
+	</div>
 </div>
 
-<div style="margin-top: 1rem;">
-	<button on:click={sendGet}>Get</button>
-	<p>Ответ: {responseText}</p>
-</div>
+
+<style>
+	.main{
+		text-align: center;
+		position: relative;
+        padding: 3rem;
+        text-align: center;
+        margin-top: 15rem;
+        width: 450px;
+        margin-left: auto;
+        margin-right: auto;
+		font-family: "Roboto", sans-serif;
+	}
+
+	h2{
+		font-size: 1,2rem;
+		color: black;
+	}
+
+	.input-container {
+		position: relative;
+		width: 100%;
+		margin-top: 1rem;
+	}
+
+	input{
+		width: 100%;
+		height: 50px;
+		background-color: #f3f3f3;
+		border-radius: 23px;
+		padding: 0 50px 0 20px;
+		border: none;
+		outline: none;
+		font-size: 1rem;
+		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+	}
+
+	.send-button {
+		position: absolute;
+		right: 1rem;
+		top: 50%;
+		transform: translateY(-50%);
+		background: none;
+		border: none;
+		cursor: pointer;
+		padding: 5px;
+	}
+
+	.send-button img {
+		width: 24px;
+		height: 24px;
+	}
+
+</style>
